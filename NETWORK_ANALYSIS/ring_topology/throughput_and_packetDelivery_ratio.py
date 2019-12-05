f213a98457f5eabc659d4481d81f4ec31c501de6
aman212yadav <31543527+aman212yadav@users.noreply.github.com>
2019-12-05 18:45:49 +0530

    Add files via upload

---
+++ NETWORK_ANALYSIS/ring_topology/throughput_and_packetDelivery_ratio.py
@@ -0,0 +1,23 @@
+total_packets_received=0
+total_generated_packets=0
+start_time=0
+end_time=0
+with open('ring_topology.tr') as f:
+     for line in f:
+         p=line.split()
+         for i in range(len(p)):
+             if i==1:
+                if total_packets_received==0:
+                   start_time=float(p[i])
+                else:
+                   end_time=float(p[i])
+             if i==0 and p[i]=='r':
+                total_packets_received+=1
+         total_generated_packets+=1
+   
+print('total packets received : ',total_packets_received)
+print('total packets generated : ',total_generated_packets)
+print('packet delivery ratio : ',total_packets_received/total_generated_packets)
+print('throughput : ',total_packets_received/(end_time-start_time)*1000)
+
+ 
